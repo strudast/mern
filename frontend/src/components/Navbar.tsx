abf957042fc0200a2133159833bdd1173fe6b01f
Giorgis Iordanidis <g.iordanidis@iit.demokritos.gr>
2025-04-08 19:40:45 +0300

    cleaned unused imports and variables

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,11 +10,10 @@ import { PlusSquareIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
-import { useProductStore } from "../store/product";
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const { products } = useProductStore();
+
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
